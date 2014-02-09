00ac301f6d624856b8ad68bb00868228713b9915
Hassan Shamim <hassan.has.you@gmail.com>
2014-02-09 00:24:04 -0600

    Use convert functional tests to TestCase

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,37 @@
 from selenium import webdriver
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+import unittest
 
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(10)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_new_visitor_visits_site(self):
+
+        # Yousuf has heard about my cool new online app.  He goes to check out its homepage
+
+        # He notices the page title and header mention the app name
+        self.assertIn('Together', self.browser.title)
+        self.fail('Finish the test!')
+
+        # He is invited to sign up.
+
+        # He clicks the 'Sign up' link
+
+        # He is taken to the SignUp page
+
+        # He sees he can sign up via his Facebook account
+
+        # He does so
+
+        # He now sees 'Yousuf' on the page
+
+        # He is happy for now, and goes and plays bass
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
